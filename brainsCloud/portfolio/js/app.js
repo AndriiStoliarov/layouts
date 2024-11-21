076c88bd3159c4acd50163b46bc72dc5e9be5472
Andrii Stoliarov <andrey159852@gmail.com>
2024-11-21 21:22:03 +0200

    part 12, 00:06:00

--- brainsCloud/portfolio/js/app.js
+++ brainsCloud/portfolio/js/app.js
@@ -1,3 +1,45 @@
 $(function() {
+
+
+    /* Filter
+    =====================*/
+    let filter = $("[data-filter]");
+
+    filter.on("click", function(event) {
+        event.preventDefault();
+
+        let cat = $(this).data('filter');
+
+        if(cat == 'all') {
+            $("[data-cat]").removeClass("hide");
+        } else {
+            $("[data-cat]").each(function() {
+
+                let workCat = $(this).data('cat');
     
+                if(workCat != cat) {
+                    $(this).addClass('hide');
+                } else {
+                    $(this).removeClass('hide');
+                }
+            });
+        }
+    });
+
+
+    /* Modal
+    =====================*/
+    const modalCall = $("[data-modal]");
+
+    modalCall.on("click", function(event){
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId)
+
+        console.log(modalId);
+    });
+
 });
