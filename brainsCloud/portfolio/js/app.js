13389299ae9b5964512166b62dc5c61bf9b1bc46
Andrii Stoliarov <andrey159852@gmail.com>
2024-11-22 18:44:34 +0200

    part 13, 00:24:00

--- brainsCloud/portfolio/js/app.js
+++ brainsCloud/portfolio/js/app.js
@@ -30,6 +30,7 @@ $(function() {
     /* Modal
     =====================*/
     const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
 
     modalCall.on("click", function(event){
         event.preventDefault();
@@ -37,9 +38,74 @@ $(function() {
         let $this = $(this);
         let modalId = $this.data('modal');
 
-        $(modalId)
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
 
-        console.log(modalId);
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+
+        // переращет размеров
+        $('#worksSlider').slick('setPosition');
     });
 
+    modalClose.on("click", function(event){
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+        modalParent.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal").on("click", function(event){
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event){
+        event.stopPropagation();
+    });
+
+
+    /* Slider: https://kenwheeler.github.io/slick/
+    =====================*/
+
+    $('#worksSlider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+    $(".slickPrev").on("click", function(event) {
+        event.preventDefault();
+
+        $('#worksSlider').slick("slickPrev");
+    });
+
+    $(".slickNext").on("click", function(event) {
+        event.preventDefault();
+
+        $('#worksSlider').slick("slickNext");
+    });
 });
